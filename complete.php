07561ae0eaeca68da38a9472adbafa83efc47e83
swarrier16 <swarrier16@gmail.com>
2017-04-27 13:24:34 -0400

    added complete function

---
+++ complete.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+$host="localhost"; // Host name 
+$username="root"; // Mysql username 
+$password=""; // Mysql password 
+$db_name="ssw7"; // Database name 
+$tbl_name="user"; // Table name 
+
+
+// Connect to server and select database.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+
+
+// $complete = $_GET['complete'];
+
+ 
+
+// if(isset($_POST['continue'])) {
+// 	"UPDATE tasks SET complete = 1 WHERE complete = 0";    
+//    }
+
+$sql = 'SELECT * FROM `tasks` WHERE complete = 0 ORDER BY due ASC';
+
+if(isset($_POST['complete'])) {
+   $sql = "UPDATE tasks SET complete = 1 WHERE complete = 0";
+   header("Location: tasks.php");
+  }
+
+
+?>
