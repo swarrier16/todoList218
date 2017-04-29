c13c79f579c4b1003674d58315b83e0d3bae3e36
swarrier16 <swarrier16@gmail.com>
2017-04-29 13:42:56 -0400

    Finish complete function

--- complete.php
+++ complete.php
@@ -1,32 +1,18 @@
 <?php
 
+require_once 'database.php';
 
 
-$host="sql2.njit.edu"; // Host name
-$username="ssw7"; // Mysql username
-$password="SUbO2ykc4"; // Mysql password
-$db_name="ssw7"; // Database name
-$tbl_name="user"; // Table name
+$task_id = $_POST['task_id'];
+$sql = 'SELECT complete FROM `tasks` WHERE complete = 0 ORDER BY due ASC';
 
-
-// Connect to server and select database.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
-
-
-// $complete = $_GET['complete'];
-
- 
-
-// if(isset($_POST['continue'])) {
-// 	"UPDATE tasks SET complete = 1 WHERE complete = 0";    
-//    }
-
-$sql = 'SELECT * FROM `tasks` WHERE complete = 0 ORDER BY due ASC';
-
-if(isset($_POST['complete'])) {
-   $sql = "UPDATE tasks SET complete = 1 WHERE complete = 0";
-   header("Location: tasks.php");
+if(isset($_POST['complete'])) 
+{
+   $sql = "UPDATE tasks SET complete = 1 WHERE id = '$task_id' ";
+   $statement = $db->prepare($sql);
+	$statement->execute();
+	$statement->closeCursor();
+   include 'tasks.php';
   }
 
 
