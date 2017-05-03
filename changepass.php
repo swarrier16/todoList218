fec7d64f68439f387100c5e09e1dd008c35d8b6f
swarrier16 <swarrier16@gmail.com>
2017-05-03 16:41:18 -0400

    Add Password Reset function

--- changepass.php
+++ changepass.php
@@ -6,30 +6,37 @@ $password=""; // Mysql password
 $db_name="ssw7"; // Database name 
 $tbl_name="user"; // Table name 
 
-$conn = new mysqli($host, $username, $password, $db_name);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
 
+ $db = new PDO("mysql:host=localhost;dbname=ssw7", $username, $password);
+ 
+$email = $_POST['email'];
+$password1 = $_POST['password1'];
+$password2 = $_POST['password2'];
+$checkUserID = mysql_query("SELECT * from user WHERE email = '$email'");
+$checkUserpass = mysql_query("SELECT * from user WHERE password = '$password1'");
 
-if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-		$email = $_POST["email"];
-		
-}else{
-	echo "email is not valid";
-	exit;
-}
+	if (!$checkUserID) {
+	    die('Query failed to execute for some reason');
+	}
 
-
-$sql = "UPDATE user SET password = $password";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
-}
-
-$conn->close();
+	if((mysql_num_rows($checkUserID)>=1) && ($password1 == $password2)){
+		$sql = "UPDATE user SET password = '$password1' WHERE email = '$email'";
+		$statement = $db->prepare($sql);
+		$statement->execute();
+		$statement->closeCursor();	
+		include 'index.php';
+	  
+	} else if ($email == '') {
+	  $message = "Please enter your email\\nTry again.";
+	  echo "<script type='text/javascript'>alert('$message');</script>";
+	  include 'newpass.php';
+	}
+	else if ((mysql_num_rows($checkUserID) >= 1) && ($password1 != $password2)) {
+	  $message = "Passwords do not match.\\nTry again.";
+	  echo "<script type='text/javascript'>alert('$message');</script>";
+	  include 'newpass.php';
+	 }
 
 ?>
